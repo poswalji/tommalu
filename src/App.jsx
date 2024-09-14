24f8442336262f9374b718737d3ec87cd153978d
Malu Ram <maluramgurjar64@gmail.com>
2024-09-15 01:44:21 +0530

    placeorder

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/cart' element={<Cart/>}/>
-      <Route path='/order' element={<PlaceOrder/>}/>
+      
      </Routes>
      <AppDownload/>
     </div>
